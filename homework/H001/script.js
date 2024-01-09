c3f70b63ab4d920b6764ad1c636e42936f3aefb7
David <david@akunts.ru>
2024-01-09 21:28:33 +0300

    hw1

--- homework/H001/script.js
+++ homework/H001/script.js
@@ -1,23 +1,33 @@
 const http = require("http");
+let counterMain = 0;
+let counterAbout = 0;
+let counter404 = 0;
 
 const server = http.createServer((req, res) => {
   console.log("Запрос получен");
 
   if (req.url === "/") {
+    ++counterMain;
     res.writeHead(200, {
       "Content-type": "text/html; charset=UTF-8",
     });
-    res.end("<h1>Main Page</h1>");
+    res.end(
+      `<h1>Main Page</h1><br><p>You have been here ${counterMain} times</p><br><a href="http://127.0.0.1:3000/about">Go to about →</a>`
+    );
   } else if (req.url === "/about") {
+    ++counterAbout;
     res.writeHead(200, {
       "Content-type": "text/html; charset=UTF-8",
     });
-    res.end("<h1>About Page</h1>");
+    res.end(
+      `<h1>About Page</h1><br><p>You have been here ${counterAbout} times</p><br><a href="http://127.0.0.1:3000/">Go to main →</a>`
+    );
   } else {
+    ++counter404;
     res.writeHead(404, {
       "Content-type": "text/html; charset=UTF-8",
     });
-    res.end("<h1>404</h1>");
+    res.end(`<h1>404 not found</h1><br><p>You have been here ${counter404} times</p>`);
   }
 });
 
